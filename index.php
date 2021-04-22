01f03f49100644198434bfccc7802c834765c223
tynzn <tynznt@gmail.com>
2021-04-22 20:26:37 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,372 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400" rel="stylesheet" />
+    <link href="fontawesome/css/all.min.css" rel="stylesheet" />
+    <link href="css/templatemo-comparto.css" rel="stylesheet" />
+    <title>Comparto HTML CSS Template</title>
+<!--
+
+Comparto TemplateMo
+
+https://templatemo.com/tm-544-comparto
+
+-->
+</head>
+
+<body>
+    <!-- Back to top button -->
+    <a id="button"><i class="fas fa-angle-up fa-2x"></i></a>
+    <div class="container-fluid">
+        <div class="tm-site-header tm-mb-1">
+            <div class="tm-site-name-container tm-bg-color-1">
+                <h1 class="tm-text-white">Bee Hive Collections </h1>
+            </div>
+            <div class="tm-nav-container tm-bg-color-8">
+                <nav class="tm-nav" id="tm-nav">
+                    <ul>
+                        <!--
+                        <li class="tm-nav-item current">
+                            <a href="#about" class="tm-nav-link">
+                                <span class="tm-mb-1">.01</span>
+                                <span>About</span>
+                            </a>
+                        </li>
+                        <li class="tm-nav-item">
+                            <a href="#services" class="tm-nav-link">
+                                <span class="tm-mb-1">.02</span>
+                                <span>Services</span>
+                            </a>
+                        </li>
+                        <li class="tm-nav-item">
+                            <a href="#gallery" class="tm-nav-link">
+                                <span class="tm-mb-1">.03</span>
+                                <span>Gallery</span>
+                            </a>
+                        </li>
+                        <li class="tm-nav-item">
+                            <a href="#contact" class="tm-nav-link">
+                                <span class="tm-nav-text tm-mb-1">.04</span>
+                                <span class="tm-nav-text">Contact</span>
+                            </a>
+                        </li>
+                        -->
+                    </ul>
+                </nav>
+            </div>
+        </div>
+        <section class="tm-mb-1" id="about">
+            <div class="tm-row tm-about-row">
+                <div class="tm-section-1-l">
+                    <img src="img/hublot1-.jpg" alt="About image" class="tm-img-responsive">
+                </div>
+                <article class="tm-section-1-r tm-bg-color-8">
+                    <h2 class="tm-mb-2 tm-title-color">Hublot 2021 Collection</h2>
+                    <p><a rel="nofollow" href="#" target="_parent"></a>The Best Thing to Happen to Watch Since Hublot</p>
+                    <p>Sleek Design || Fully Waterproof || Quality rose Gold Watch with Black Design <a rel="nofollow" href="#" target="_parent"></br>
+                    </br>So much Quality For Less </a> </br>
+                </br>Limited Stock Available on Bee Hive Store.</p>
+                    <p>Original And durable watch buckle</p>
+                    
+                    <a href="#services" class="tm-btn tm-btn-1 tm-link-to-services">ORDER NOW</a>
+                </article>
+            </div>
+         <section class="tm-bg-color-4 tm-mb-3 tm-gallery-section" id="gallery">
+                <div class="tm-gallery-header">
+                    <h2 class="tm-mb-1 ">Sleek style As it goes with corporate and traditional attire as seen in the images below</h2>
+                </div>
+                <div class="tm-gallery-outer">
+                    <div class="tm-gallery" id="tm-gallery">
+                       
+                            <div class="tm-gallery-item animals">
+                                <figure class="effect-bubba">
+                                    <img src="img/hublot15-.jpg" alt="Gallery item" class="tm-img-responsive" />
+                                    <figcaption>
+                                        <h2>Pro <span>Display</span></h2>
+                                        <p>You can make your artwork gallery.</p>
+                                        <a href="#">View more</a>
+                                    </figcaption>
+                                </figure>
+                            </div>
+                            <div class="tm-gallery-item peopl">
+                                <figure class="effect-bubba">
+                                    <img src="img/hublot16-.jpg" alt="Gallery item" class="tm-img-responsive" />
+                                    <figcaption>
+                                        <h2>The <span>Nature</span></h2>
+                                        <p>You can create your own HTML website.</p>
+                                        <a href="#">View more</a>
+                                    </figcaption>
+                                </figure>
+                            </div>
+                            <div class="tm-gallery-item people">
+                                <figure class="effect-bubba">
+                                    <img src="img/hublot17-.jpg" alt="Gallery item" class="tm-img-responsive" />
+                                    <figcaption>
+                                        <h2>Color <span>Art</span></h2>
+                                        <p>You can create your own CSS website.</p>
+                                        <a href="#">View more</a>
+                                    </figcaption>
+                                </figure>
+                            </div>
+                            <div class="tm-gallery-item people">
+                                <figure class="effect-bubba">
+                                    <img src="img/hublot18-.jpg" alt="Gallery item" class="tm-img-responsive" />
+                                    <figcaption>
+                                        <h2>Take it <span>easy</span></h2>
+                                        <p>You can create your own art gallery.</p>
+                                        <a href="#">View more</a>
+                                    </figcaption>
+                                </figure>
+                            </div>
+                            
+                        </div>
+                    </div>
+    </section>
+
+
+        </section>
+        <div class="tm-bg-color-1 tm-mb-1 tm-row tm-social-row">
+            <div class="tm-icon">
+                <div class="tm-icon-inner">
+                    <a href="#services">
+                        <i class="fas fa-bus fa-4x tm-mb-1"></i>
+                        <p>Free Delivery within lagos 2-3 work days</p>
+                    </a>
+                </div>
+            </div>
+            <div class="tm-icon">
+                <div class="tm-icon-inner">
+                    <a href="#gallery">
+                        <i class="fas fa-money-bill fa-4x tm-mb-1"></i>
+                        <p>Cash on Delivery </p>
+                    </a>
+                </div>
+            </div>
+            <div class="tm-icon">
+                <div class="tm-icon-inner">
+                    <a href="#contact">
+                        <i class="fas fa-images fa-4x tm-mb-1"></i>
+                        <p>30 days money back guarantee</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <section class="tm-mb-1 tm-row tm-services-row" id="services">
+           
+            <div class="tm-section-2-r">
+                <img src="img/hublot14-.jpg" alt="Services image" class="tm-img-responsive">
+            </div>
+            <div class="tm-section-2-l">
+                <article class="tm-bg-color-6 tm-box-pad tm-mb-1">
+                    <h2 class="tm-mb-2">Customer's Feedback</h2>
+                    <p><i>"I really like this watch concept and am definately wearing it to my top events, the Merchant are reliable and i recommend them 100% " </i></p>
+                    <p class="tm-mb-0">Mr Tunde. Surulere Lagos</p>
+                </article>
+                <div class="tm-bg-color-7 tm-em-box">
+                    <p class="tm-text-color-2"><i>So a decided to gift my Boo this timepiece for his birthday and he would not stop thanking me another thing is i really give it to your customer care team at Bee Hive, you guys rock</i></p> <p>Mrs Blessing C. Ejigbo.lagos</p>
+                    <a href="#gallery" class="tm-btn tm-btn-2">Order Yours While Stock Last</a>
+                </div>
+            </div>
+           
+        </section>
+        <section class="tm-bg-color-4 tm-mb-3 tm-gallery-section" id="gallery">
+            <div class="tm-gallery-header">
+                <h2 class="tm-mb-1 ">.Authentic Feature lookout</h2>
+                 <!--
+                <ul class="tm-gallery-filter tabs clearfix filters-button-group">
+                   
+                    <li><a role="button" href="#" class="active" data-filter="*">Show All</a></li>
+                    . <li><a role="button" href="#" data-filter=".nature">Nature</a></li>
+                    . <li><a role="button" href="#" data-filter=".animals">Animals</a></li>
+                    . <li><a role="button" href="#" data-filter=".people">People</a></li>
+                </ul>-->
+            </div>
+            <div class="tm-gallery-outer">
+                <div class="tm-gallery" id="tm-gallery">
+                    <div class="tm-gallery-item nature">
+                        <figure class="effect-bubba">
+                            <img src="img/hublot4-.jpg" alt="Gallery item" class="tm-img-responsive" />
+                            <figcaption>
+                                <h2>Masculine <span></span></h2>
+                                <p>A watch for the true man essence.</p>
+                                <a href="#">Order Now</a>
+                            </figcaption>
+                        </figure>
+                    </div>
+                    <div class="tm-gallery-item animals">
+                        <figure class="effect-bubba">
+                            <img src="img/hublot6-.jpg" alt="Gallery item" class="tm-img-responsive" />
+                            <figcaption>
+                                <h2>Back Shot</h2>
+                                <p>Giving You an authentic watch look.</p>
+                                <a href="#">View more</a>
+                            </figcaption>
+                        </figure>
+                    </div>
+                    <div class="tm-gallery-item nature">
+                        <figure class="effect-bubba">
+                            <img src="img/hublot7-.jpg" alt="Gallery item" class="tm-img-responsive" />
+                            <figcaption>
+                                <h2>Authentic <span>Buckle</span></h2>
+                                <p>.</p>
+                                <a href="#">Order Now</a>
+                            </figcaption>
+                        </figure>
+                    </div>
+                    <div class="tm-gallery-item nature">
+                        <figure class="effect-bubba">
+                            <img src="img/hublot11-.jpg" alt="Gallery item" class="tm-img-responsive" />
+                            <figcaption>
+                                <h2>Originality  <span>Tag</span></h2>
+                                <p>Check out the authentic tag.</p>
+                                <a href="#">Order Now</a>
+                            </figcaption>
+                        </figure>
+                    </div>
+                    <div class="tm-gallery-item animals">
+                        <figure class="effect-bubba">
+                            <img src="img/hublot9-.jpg" alt="Gallery item" class="tm-img-responsive" />
+                            <figcaption>
+                                <h2>waterproof <span>sealed </span></h2>
+                                <p>.</p>
+                                <a href="#">Order Now</a>
+                            </figcaption>
+                        </figure>
+                    </div>
+                   
+        </section> <!-- Gallery -->
+        <section id="contact" class="tm-bg-color-5 tm-mb-3">
+            <h2 class="tm-text-white tm-contact-title">Fill The Form Below to Place Your Order</h2>
+            
+                <div class="contact-form-outer">
+                    <form id="contact-form" action="" method="POST" class="tm-bg-color-6 tm-contact-form">
+                        <div class="form-group">
+                            <input type="text" name="name" class="form-control" placeholder="Name" required="" />
+                        </div>
+                        <div class="form-group">
+                            <input type="email" name="email" class="form-control" placeholder="Email" required="" />
+                        </div>
+                        <div class="form-group">
+                            <textarea rows="4" name="message" class="form-control" placeholder="Message..."
+                                required=""></textarea>
+                        </div>
+                        <div>
+                            <button type="submit" class="ml-auto tm-btn tm-btn-3">
+                                Send
+                            </button>
+                        </div>
+                    </form>
+                </div>
+                
+            </div>
+        </section>
+        
+        <footer class="text-center tm-mb-1">
+            <p>Bee Hive Collections &copy; 2021 YESG Technologies Limited 
+            
+            - Design: <a rel="nofollow noopener" href="#" target="_blank">yeagit</a></p>
+        </footer>
+    </div> <!-- .container -->
+    <script src="js/jquery.min.js"></script> <!-- https://jquery.com/download/ -->
+    <script src="js/imagesloaded.pkgd.min.js"></script> <!-- https://imagesloaded.desandro.com/ -->
+    <script src="js/isotope.pkgd.min.js"></script> <!-- https://isotope.metafizzy.co/ -->
+    <script src="js/jquery.singlePageNav.min.js"></script> <!-- https://github.com/ChrisWojcik/single-page-nav -->
+    <script>
+
+        // Scroll to Top button
+        var btn = $('#button');
+
+        $(window).scroll(function () {
+            if ($(window).scrollTop() > 300) {
+                btn.addClass('show');
+            } else {
+                btn.removeClass('show');
+            }
+        });
+
+        btn.on('click', function (e) {
+            e.preventDefault();
+            $('html, body').animate({ scrollTop: 0 }, '300');
+        });
+
+        // DOM is ready
+        $(function () {
+            // Single Page Nav
+            $('#tm-nav').singlePageNav({ speed: 600 });
+
+            // Smooth Scroll (https://css-tricks.com/snippets/jquery/smooth-scrolling/)
+            $('a[href*="#"]')
+                // Remove links that don't actually link to anything
+                .not('[href="#"]')
+                .not('[href="#0"]')
+                .click(function (event) {
+                    // On-page links
+                    if (
+                        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+                        &&
+                        location.hostname == this.hostname
+                    ) {
+                        // Figure out element to scroll to
+                        var target = $(this.hash);
+                        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                        // Does a scroll target exist?
+                        if (target.length) {
+                            // Only prevent default if animation is actually gonna happen
+                            event.preventDefault();
+                            $('html, body').animate({
+                                scrollTop: target.offset().top
+                            }, 600, function () {
+                                // Callback after animation
+                                // Must change focus!
+                                var $target = $(target);
+                                $target.focus();
+                                if ($target.is(":focus")) { // Checking if the target was focused
+                                    return false;
+                                } else {
+                                    $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                                    $target.focus(); // Set focus again
+                                };
+                            });
+                        }
+                    }
+                });
+
+            /* Isotope Gallery */
+
+            // init isotope
+            var $gallery = $(".tm-gallery").isotope({
+                itemSelector: ".tm-gallery-item",
+                layoutMode: "fitRows"
+            });
+            // layout Isotope after each image loads
+            $gallery.imagesLoaded().progress(function () {
+                $gallery.isotope("layout");
+            });
+
+            $(".filters-button-group").on("click", "a", function () {
+                var filterValue = $(this).attr("data-filter");
+                $gallery.isotope({ filter: filterValue });
+            });
+
+            $(".tabgroup > div").hide();
+            $(".tabgroup > div:first-of-type").show();
+            $(".tabs a").click(function (e) {
+                e.preventDefault();
+                var $this = $(this),
+                    tabgroup = "#" + $this.parents(".tabs").data("tabgroup"),
+                    others = $this
+                        .closest("li")
+                        .siblings()
+                        .children("a"),
+                    target = $this.attr("href");
+                others.removeClass("active");
+                $this.addClass("active");
+            });
+        });
+    </script>
+</body>
+</html>
