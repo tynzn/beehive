2be4b907345288ec24b12b8712638a3c20effff1
tynzn <tynznt@gmail.com>
2021-05-06 16:00:16 +0100

    timer formart

--- index.php
+++ index.php
@@ -43,7 +43,6 @@ https://templatemo.com/tm-544-comparto
     <a id="button"><i class="fas fa-angle-up fa-2x"></i></a>
     <div class="container-fluid">
         <div class="tm-site-header tm-mb-1">
-        
             <div class="tm-site-name-container tm-bg-color-1">
                 <h1 class="tm-text-white">Bee Hive Collections </h1>
             </div>
@@ -81,7 +80,6 @@ https://templatemo.com/tm-544-comparto
                 </nav>
             </div>
         </div>
-        <div class="elfsight-app-c153f966-ca94-4512-a80a-f0f66c9715c4"></div>
         <section class="tm-mb-1" id="about">
             <div class="tm-row tm-about-row">
                 <div class="tm-section-1-l">
@@ -381,6 +379,6 @@ https://templatemo.com/tm-544-comparto
                 $this.addClass("active");
             });
         });
-        <script src="https://apps.elfsight.com/p/platform.js" defer></script>
-<div class="elfsight-app-c153f966-ca94-4512-a80a-f0f66c9715c4"></div>
+    </script><script src="https://apps.elfsight.com/p/platform.js" defer></script>
+<div class="elfsight-app-c153f966-ca94-4512-a80a-f0f66c9715c4"></div>></body>
 </html>
