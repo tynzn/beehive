cb7f56ab65e3a009b61b3fc91e0b37a049593619
tynzn <tynznt@gmail.com>
2021-05-09 10:39:37 +0100

    new facebook pixel

--- index.php
+++ index.php
@@ -21,6 +21,8 @@ https://templatemo.com/tm-544-comparto
 
 <!-- Facebook Pixel Code -->
 <script>
+<!-- Facebook Pixel Code -->
+<script>
 !function(f,b,e,v,n,t,s)
 {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
 n.callMethod.apply(n,arguments):n.queue.push(arguments)};
@@ -29,11 +31,11 @@ n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '2575885192535179');
+fbq('init', '1224264694828563');
 fbq('track', 'PageView');
 </script>
 <noscript><img height="1" width="1" style="display:none"
-src="https://www.facebook.com/tr?id=2575885192535179&ev=PageView&noscript=1"
+src="https://www.facebook.com/tr?id=1224264694828563&ev=PageView&noscript=1"
 /></noscript>
 <!-- End Facebook Pixel Code -->
 </head>
